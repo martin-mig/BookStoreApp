fe833de400bdd2474350090cb7c3404e90e35566
patodimasi <pdimasi@hotmail.com>
2024-02-02 18:10:07 -0300

    Agregado de search para los productos en sales limpieza de codigo con warning por consola

--- client/src/components/Button/VentasC.js
+++ client/src/components/Button/VentasC.js
@@ -10,6 +10,7 @@ import { Panel } from 'primereact/panel';
 import { Badge } from 'primereact/badge';
 import { Toast } from 'primereact/toast';
 import { useNavigate } from 'react-router-dom';
+import { InputText } from 'primereact/inputtext';
 
 export const VentasC = () => {
     const [ajaxUrl, SetAjaxUrl] = useState(process.env.REACT_APP_API + "/search-books");
@@ -27,6 +28,9 @@ export const VentasC = () => {
     const [carritoP, setCarritoP] = useState([]);
     const [contador, setContador] = useState(0);
     const [sumaPrecio, setSumaPrecio] = useState(0);
+   
+    const [filtro, setFiltro] = useState('');
+    const [productosFiltrados, setProductosFiltrados] = useState([]);
 
     const getProducts = async() =>{
         const response = await postData({});
@@ -47,6 +51,14 @@ export const VentasC = () => {
         }   
     },[]);
  
+    useEffect(() => {
+        // Lógica para filtrar productos cuando cambia el filtro.
+        let regex = new RegExp(`^${filtro}`, 'i');
+        let approved = products.filter(product => regex.test(product.title));
+      //  console.log("ES EL APPROVED " , approved);
+        setProductosFiltrados(approved);
+    }, [filtro, products]); 
+
     const toast = useRef(null);
 
     const navigate = useNavigate();
@@ -90,12 +102,30 @@ export const VentasC = () => {
         navigate('/consultas/finalizar-compra', { state: { sumaPrecio,carritoP} });
     } 
 
+    //const filtrarProd = (e) =>{
+       /* let filtro = e.target.value;
+        let regex = new RegExp(`^${filtro}`, 'i');
+        console.log("valor del filtro", e.target.value);
+
+       
+        let approved = products.filter(product => regex.test(product.title));
+
+        getProducts(approved);
+        console.log("soon los parecidos " , approved);
+        */
+      //  setFiltro(e.target.value);
+   // }
+
     const headerT = (options) => {
         const className = `${options.className} justify-content-space-between`;
         return (
-            <div className={className}>
+            <div className={className} >
                 <div className="flex align-items-center gap-2">
-                    <Dropdown options={sortOptions} value={sortKey} optionLabel="label" placeholder="Sort By Price" onChange={onSortChange} className="flex justify-start w-full sm:w-14rem" />
+                        <Dropdown options={sortOptions} value={sortKey}   optionLabel="label" placeholder="Sort By Price" onChange={onSortChange} className="flex justify-start w-full sm:w-14rem" />             
+                    <span className="p-input-icon-left">
+                    <i className="pi pi-search" />
+                        <InputText type="search"  onChange={(e) => setFiltro(e.target.value)}  placeholder="Search Title..." />
+                    </span>
                 </div>
                 <div className="grid">
                     <div className="col">
@@ -184,28 +214,28 @@ export const VentasC = () => {
     const itemTemplate = (product) => {
         return(
             <>     
-                    <div className="col-12">
-                        <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
-                        <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round"  src={require('../../images/' + product.categories + '.png')} />
-                            <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
-                                <div className="flex flex-column align-items-center sm:align-items-start gap-3">
-                                    <div className="text-2xl font-bold text-900">{product.title}</div>
-                                    <Rating value={product.rating} readOnly cancel={false}></Rating>
-                                    <div className="flex align-items-center gap-3">
-                                        <span className="flex align-items-center gap-2">
-                                            <i className="pi pi-tag"></i>
-                                            <span className="font-semibold">{product.categories}</span>
-                                        </span>
-                                        {stockBodyTemplate(product)}
-                                    </div>
+                <div className="col-12">
+                    <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
+                    <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round"  src={require('../../images/' + product.categories + '.png')} />
+                        <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+                            <div className="flex flex-column align-items-center sm:align-items-start gap-3">
+                                <div className="text-2xl font-bold text-900">{product.title}</div>
+                                <Rating value={product.rating} readOnly cancel={false}></Rating>
+                                <div className="flex align-items-center gap-3">
+                                    <span className="flex align-items-center gap-2">
+                                        <i className="pi pi-tag"></i>
+                                        <span className="font-semibold">{product.categories}</span>
+                                    </span>
+                                    {stockBodyTemplate(product)}
                                 </div>
-                                <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
-                                    <span className="text-2xl font-semibold">${product.price}</span>
-                                    <Button icon="pi pi-shopping-cart" className="p-button-rounded"  onClick={()=> btnCarrito(product)} ></Button>
-                                </div>
+                            </div>
+                            <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
+                                <span className="text-2xl font-semibold">${product.price}</span>
+                                <Button icon="pi pi-shopping-cart" className="p-button-rounded"  onClick={()=> btnCarrito(product)} ></Button>
                             </div>
                         </div>
                     </div>
+                </div>
                 
             </>
         )
@@ -213,10 +243,10 @@ export const VentasC = () => {
     return (
         <div>
             <Toast ref={toast} />
-            <Panel headerTemplate={headerT} >
+            <Panel headerTemplate={headerT}>
                 <div className='grid'>
                     <div className="col-9" >
-                        <DataView value={products} itemTemplate={itemTemplate} paginator rows={3} sortField={sortField} sortOrder={sortOrder}/>
+                        <DataView value={productosFiltrados} itemTemplate={itemTemplate} paginator rows={3} sortField={sortField} sortOrder={sortOrder}/>
                     </div>
                     <div className="col-3" style={{ marginTop: '30px' }}>
                         <Carrito datoCarrito={carritoP} setdatoCarrito={setCarritoP} contadorBadger={contador} setBadgerContador={setContador} msj={showSticky}/>
